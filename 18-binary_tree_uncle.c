71ba62dadf785c4fb3b6e5302b61fb83d8d9e5c8
Tishlyy <ekaetteessien27@gmail.com>
2022-08-26 02:42:49 +0100

    Uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,3 +6,17 @@
  *
  * Return: the uncle of a given node
  */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *left = NULL;
+	binary_tree_t *right = NULL;
+	binary_tree_t *parent = (node != NULL ? node->parent : NULL);
+
+	if ((parent != NULL) && (parent->parent != NULL))
+	{
+		left = parent->parent->left;
+		right = parent->parent->right;
+	}
+	return (left == parent ? right : left);
+}
