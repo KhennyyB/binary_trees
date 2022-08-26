95157daef6e0367495e8b352b8ab43809edeb035
Tishlyy <ekaetteessien27@gmail.com>
2022-08-26 01:30:20 +0100

    Perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,13 @@
+#include "binry_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if tree is perfect
+ * @tree: pointer to root
+ *
+ * Return: 1 if perfect, else 0
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+
+}
