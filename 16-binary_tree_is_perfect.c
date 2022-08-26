29114e154320a4659f0237b3656eb23e5159b1ca
Tishlyy <ekaetteessien27@gmail.com>
2022-08-26 01:50:08 +0100

    Perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,36 @@
 #include "binry_trees.h"
 
 /**
+ *
+ *
+ *
+ */
+
+void tree_stats(const binary_tree_t *tree, size_t n,
+		size_t *leaf, sie_t *height)
+{
+	if (tree!= NULL)
+	{
+		if ((tree->left == NULL) && (tree->right == NULL))
+		{
+			if (leaves != NULL)
+			{
+				(*leaves)++;
+			}
+			if ((height != NULL) && (n > *height))
+			{
+				*height = n;
+			}
+		}
+		else
+		{
+			tree_stats(tree->left, n + 1, leaves, height);
+			tree_stats(tree->right, n + 1, leaves, height);
+		}
+	}
+}
+
+/**
  * binary_tree_is_perfect - checks if tree is perfect
  * @tree: pointer to root
  *
